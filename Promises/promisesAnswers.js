432f6509465f605664a585d59ce7b18fe4caad1a
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-01-22 21:14:13 +0300

    add promise answers

--- Promises/promisesAnswers.js
+++ Promises/promisesAnswers.js
@@ -7,8 +7,26 @@ function sayHello() {
 // Uncomment the line below when ready
 sayHello(); // should log "Hello" after 1000ms
 
-
 // Challenge 2
 var promise = new Promise(function (resolve, reject) {
   setTimeout(resolve, 1000);
 });
+
+// Challenge 3
+
+promise = new Promise(function (resolve, reject) {
+  reject("Rejected!");
+});
+
+// Should print out "Reject!"
+promise.catch((error) => console.log(error));
+
+// Challenge 4
+
+promise = new Promise(function (resolve, reject) {
+  resolve();
+});
+
+// Uncomment the lines below when ready
+promise.then(() => console.log("Promise has been resolved!"));
+console.log("I'm not the promise!");
