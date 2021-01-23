cfc3d0cb8b79ad1781300fbf4fe3003c83ccda20
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-01-23 21:17:16 +0300

    add oop answers

--- OOP/oopAnswers.js
+++ OOP/oopAnswers.js
@@ -19,7 +19,32 @@ const personStore = {
   greet: function () {
     console.log("hello");
   },
+  introduce: function () {
+    console.log(`Hi, my name is ${this.name}`);
+  },
 };
 
 // /********* Uncomment this line to test your work! *********/
 // personStore.greet(); // -> Logs 'hello'
+
+/*** CHALLENGE 3 ***/
+
+function personFromPersonStore(name, age) {
+  const person = Object.create(personStore);
+  person.name = name;
+  person.age = age;
+  return person;
+}
+
+const sandra = personFromPersonStore("Sandra", 26);
+
+// /********* Uncomment these lines to test your work! *********/
+// console.log(sandra.name); // -> Logs 'Sandra'
+// console.log(sandra.age); //-> Logs 26
+// sandra.greet(); //-> Logs 'hello'
+
+/*** CHALLENGE 4 ***/
+
+// add code here
+
+sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
