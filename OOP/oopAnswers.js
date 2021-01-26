a1d37c98ad3656d6fc8ac308cc3d8bee4f746b80
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-01-26 18:57:25 +0300

    add oop answers

--- OOP/oopAnswers.js
+++ OOP/oopAnswers.js
@@ -74,3 +74,39 @@ const mike = personFromConstructor("Mike", 30);
 // console.log(mike.name); // -> Logs 'Mike'
 // console.log(mike.age); //-> Logs 30
 // mike.greet(); //-> Logs 'hello'
+
+/*** CHALLENGE 7 ***/
+
+// mike.introduce(); // -> Logs 'Hi, my name is Mike'
+
+/*** CHALLENGE 8 ***/
+class PersonClass {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log("Hello");
+  }
+}
+
+// /********* Uncomment this line to test your work! *********/
+const george = new PersonClass();
+// george.greet(); // -> Logs 'hello'
+
+/*** CHALLENGE 9 ***/
+
+class DeveloperClass extends PersonClass {
+  constructor(name, age) {
+    super(name, age);
+  }
+  introduce() {
+    console.log(`Hello World, my name is ${this.name}`);
+  }
+}
+
+// /********* Uncomment these lines to test your work! *********/
+// const thai = new DeveloperClass('Thai', 32);
+// console.log(thai.name); // -> Logs 'Thai'
+// thai.introduce(); //-> Logs 'Hello World, my name is Thai'
