d097f96e278fc0a7fadd199c2093d313668f58b8
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-01-28 20:57:10 +0300

    add HigherOrderFunctions questions and answers

---
+++ HigherOrderFunctions/HOFAnswers.js
@@ -0,0 +1,24 @@
+// Type JavaScript here and click "Run Code" or press Ctrl + s
+console.log("Hello, world!");
+
+// ##########################
+// # Higher-Order Functions #
+// ##########################
+
+// Challenge 1
+const addTwo = (num) => {
+  return num + 2;
+};
+
+// To check if you've completed this function, uncomment these console.logs!
+// console.log(addTwo(3));
+// console.log(addTwo(10));
+
+// Challenge 2
+const addS = (word) => {
+  return word + "s";
+};
+
+// Uncomment these to check your work
+// console.log(addS('pizza'));
+// console.log(addS('bagel'));
