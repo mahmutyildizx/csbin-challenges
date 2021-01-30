5d9d33faeea13d05820c3002b853ce27f0ed6929
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-01-30 18:19:24 +0300

    add higher order functions answers

--- HigherOrderFunctions/HOFAnswers.js
+++ HigherOrderFunctions/HOFAnswers.js
@@ -22,3 +22,27 @@ const addS = (word) => {
 // Uncomment these to check your work
 // console.log(addS('pizza'));
 // console.log(addS('bagel'));
+
+// Challenge 3
+const map = (array, callback) => {
+  const output = [];
+  for (let i = 0; i < array.length; i++) {
+    output.push(callback(array[i]));
+  }
+  return output;
+};
+
+// console.log(map([1, 2, 3], addTwo));
+
+// Challenge 4
+const forEach = (array, callback) => {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i]);
+  }
+};
+
+// See for yourself if your forEach works!
+let alphabet = "";
+const letters = ["a", "b", "c", "d"];
+forEach(letters, (char) => (alphabet += char));
+// console.log(alphabet);   //prints 'abcd'
